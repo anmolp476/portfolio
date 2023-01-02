b0abca17e260e5b15a2fc4803f456ecfd8750834
Anmol <anmolp476@gmail.com>
2023-01-02 18:39:44 -0500

    Styling the overall layout of the sidebar

---
+++ components/Layout.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Sidebar from './Sidebar'
+
+const Layout = () => {
+  return (
+    <div className="h-screen flex flex-row justify-start">
+        <Sidebar/>
+        <div className="bg-blue-900 flex-1 p-4 text-white border-1 border-dashed"></div>
+    </div>
+  )
+}
+
+export default Layout
