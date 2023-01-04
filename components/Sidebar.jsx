4a13dfdc5522d699ff5204f1c47abe5f0b79b689
Anmol <anmolp476@gmail.com>
2023-01-03 22:10:05 -0500

    Now clicking the menu button will collapse the side bar

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -7,8 +7,6 @@ const Sidebar = () => {
 
     const [sideBar, toggleSidebar] = useState(false);
 
-    const [toggleBars, toggleTheBars] = useState(false);
-
     const divCSS = classNames('h-screen px-4 pt-8 pb-4 bg-light flex justify-between flex-col w-80',
                     {
                         'w-80': !sideBar,
@@ -21,37 +19,38 @@ const Sidebar = () => {
         fontSize: '2em',
     }
 
-    const collapseIconStyle = "p-4 rounded bg-light-lighter absolute right-0"
+    const collapseIconStyle = classNames("p-4 rounded bg-light-lighter absolute",
+    {
+        "right-0": !sideBar,
+        "pl-2": sideBar,
+        "pt-14":sideBar
+    });
+
+    const collapseIconDivStyle = classNames("pl-1 gap-4 border border-dashed",
+    {
+        ["flex items-center"]: !sideBar,
+        ["hidden"]: sideBar,
+    })
 
     const handleCollapse = () =>
     {
         toggleSidebar(!sideBar);
     }
 
-    const mouseDetected = () =>
-    {
-        toggleTheBars(!toggleBars);
-    }
-
 
     return (
-        <div className={divCSS} 
-
-            onMouseEnter={mouseDetected}
-            onMouseLeave={mouseDetected}
-            
-            >
+        <div className={divCSS}>
             <div className='flex flex-col'>
                 <div className="border border-dashed flex items-center justify-between relative">
-                    <div className='flex items-center pl-1 gap-4 border border-dashed'>
+                    <div className={collapseIconDivStyle}>
                         <RiComputerLine style={logoIconStyle}/>
                         <span className={classNames('mt-2 text-lg font-medium text-white', {hidden:sideBar})}>
                             Logo
                         </span>
                     </div>
-                    {toggleBars && <button className={collapseIconStyle} onClick={handleCollapse}>
-                        <FaBars style={{color:"blue"}}/>
-                    </button>}
+                    <button className={collapseIconStyle} onClick={handleCollapse}>
+                        <FaBars style={{color:"blue", fontSize:"1.8em"}}/>
+                    </button>
                 </div>
             </div>
         </div>
