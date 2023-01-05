768c951d10e19c7e1042037fda7ab7446e5972e8
Anmol <anmolp476@gmail.com>
2023-01-04 22:52:15 -0500

    Installed the react-typical library which will allow me to use typing animations

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-
+import {React, useState} from 'react';
 import Layout from '../components/Layout'
 
 
@@ -6,7 +6,7 @@ export default function Home() {
 
   return (
     <Layout>
-      Home
+      
     </Layout>
   )
 }
