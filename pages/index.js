2dd61facb9f41c73aebe982e9730af1324f1cb16
Anmol <anmolp476@gmail.com>
2023-01-04 23:04:35 -0500

    Coded some of the homepage. I'll do the rest and more tomorrow.

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,27 @@
 import {React, useState} from 'react';
 import Layout from '../components/Layout'
+import Typical from 'react-typical';
 
 
 export default function Home() {
 
   return (
     <Layout>
-      
+      <div className="border border-dashed text-5xl w-3/5 absolute top-36">
+        <p>
+          Hi, <br></br>my name is Anmol<br></br>
+          I'm a{' '}
+          <Typical
+              loop={Infinity}
+              wrapper="b"
+              steps={[
+                'dev',
+                1000,
+                'designer',
+                1000,
+              ]}/>
+        </p>
+      </div>
     </Layout>
   )
 }
