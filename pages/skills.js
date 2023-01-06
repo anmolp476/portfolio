e331565788e19b7e04834705115e7d8c2fbc822a
Anmol <anmolp476@gmail.com>
2023-01-05 19:30:17 -0500

    Completed the skills section!

--- pages/skills.js
+++ pages/skills.js
@@ -1,11 +1,60 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import { FaNodeJs, FaGithub } from 'react-icons/fa';
+import { SiMongodb, SiExpress, SiReact, SiNextdotjs, SiTailwindcss } from 'react-icons/si';
+import { DiGit } from 'react-icons/di';
 
-
-const skills = () => {
+const Skills = () => {
   return (
-    <Layout>skills</Layout>
+    <Layout className="relative">
+      <div className="border border-solid">
+
+        <div className="w-3/6 absolute top-36 left-60 text-5xl">
+          <p className="">
+            My Toolbox Of <span className="text-blue-700">Skills</span> & <span className="text-blue-700">Tools</span>
+          </p>
+        </div>
+
+        <div className="grid grid-cols-4 grid-rows-2 gap-3 p-4 px-5 py-8 absolute top-48 left-56 overflow-hidden text-center">
+          <div className="flex flex-col items-center shadow-md mx-1 py-2 px-2 shadow-[#00000085] hover:scale-110 duration-500 text-6xl">
+            <SiMongodb/>
+            <p className="text-2xl">MongoDB</p>
+          </div>
+          <div className="flex flex-col items-center shadow-md shadow-[#00000085] hover:scale-110 p-2 duration-500 text-6xl">
+            <SiReact/>
+            <p className="text-2xl">React</p>
+          </div>
+          <div className="flex flex-col items-center shadow-md px-2 shadow-[#00000085] hover:scale-110 p-2 duration-500 text-6xl">
+            <SiExpress/>
+            <p className="text-2xl">Express</p>
+          </div>
+          <div className="flex flex-col items-center shadow-md px-2 shadow-[#00000085] hover:scale-110 p-2 duration-500 text-6xl">
+            <FaNodeJs/>
+            <p className="text-2xl">Node</p>
+          </div>
+          <div className="flex flex-col items-center shadow-md px-2 shadow-[#00000085] hover:scale-110 p-2 duration-500 text-6xl">
+            <SiNextdotjs/>
+            <p className="text-2xl">Next</p>
+          </div>
+          <div className="flex flex-col items-center shadow-md px-2 shadow-[#00000085] hover:scale-110 p-2 duration-500 text-6xl">
+            <SiTailwindcss/>
+            <p className="text-2xl">Tailwind</p>
+          </div>
+          <div className="flex flex-col items-center shadow-md px-2 shadow-[#00000085] hover:scale-110 p-2 duration-500 text-6xl">
+            <DiGit/>
+            <p className="text-2xl">Git</p>
+          </div>
+          <div className="flex flex-col items-center shadow-md px-2 shadow-[#00000085] hover:scale-110 p-2 duration-500 text-6xl">
+            <FaGithub/>
+            <p className="text-2xl">GitHub</p>
+          </div>
+        </div>
+
+      </div>
+    </Layout>
   )
 }
 
-export default skills
+export default Skills
+
+
