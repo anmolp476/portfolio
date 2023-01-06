b82e64bc3ed9ab6bf415834ef532666952804ec3
Anmol <anmolp476@gmail.com>
2023-01-05 20:12:32 -0500

    Fixed the errors

--- pages/skills.js
+++ pages/skills.js
@@ -6,47 +6,48 @@ import { DiGit } from 'react-icons/di';
 
 const Skills = () => {
   return (
-    <Layout className="">
-      <div className="border border-solid">
+    <Layout>
+      <div className="flex flex-col py-12 my-12 mr-0 w-4/6 items-start">
 
-        <div className="w-3/6 text-5xl">
-          <p className="">
-            My Toolbox Of <span className="text-blue-700">Skills</span> & <span className="text-blue-700">Tools</span>
-          </p>
-        </div>
-
-        <div className="grid grid-cols-4 grid-rows-2 gap-3 p-4 px-5 py-8 absolute top-48 left-56 overflow-hidden text-center">
-          <div className="flex flex-col items-center shadow-md mx-1 py-2 px-2 shadow-[#00000085] hover:scale-110 duration-500 text-6xl">
-            <SiMongodb/>
-            <p className="text-2xl">MongoDB</p>
+        <div className="flex flex-col items-center">
+          <div className="text-5xl text-center">
+            <p className="">
+              My Toolbox Of <span className="text-blue-700">Skills</span> & <span className="text-blue-700">Tools</span>
+            </p>
           </div>
-          <div className="flex flex-col items-center shadow-md shadow-[#00000085] hover:scale-110 p-2 duration-500 text-6xl">
-            <SiReact/>
-            <p className="text-2xl">React</p>
-          </div>
-          <div className="flex flex-col items-center shadow-md px-2 shadow-[#00000085] hover:scale-110 p-2 duration-500 text-6xl">
-            <SiExpress/>
-            <p className="text-2xl">Express</p>
-          </div>
-          <div className="flex flex-col items-center shadow-md px-2 shadow-[#00000085] hover:scale-110 p-2 duration-500 text-6xl">
-            <FaNodeJs/>
-            <p className="text-2xl">Node</p>
-          </div>
-          <div className="flex flex-col items-center shadow-md px-2 shadow-[#00000085] hover:scale-110 p-2 duration-500 text-6xl">
-            <SiNextdotjs/>
-            <p className="text-2xl">Next</p>
-          </div>
-          <div className="flex flex-col items-center shadow-md px-2 shadow-[#00000085] hover:scale-110 p-2 duration-500 text-6xl">
-            <SiTailwindcss/>
-            <p className="text-2xl">Tailwind</p>
-          </div>
-          <div className="flex flex-col items-center shadow-md px-2 shadow-[#00000085] hover:scale-110 p-2 duration-500 text-6xl">
-            <DiGit/>
-            <p className="text-2xl">Git</p>
-          </div>
-          <div className="flex flex-col items-center shadow-md px-2 shadow-[#00000085] hover:scale-110 p-2 duration-500 text-6xl">
-            <FaGithub/>
-            <p className="text-2xl">GitHub</p>
+          <div className=" grid grid-cols-4 grid-rows-2 gap-3 p-4 px-5 py-8 top-48 left-56 overflow-hidden text-center">
+            <div className="flex flex-col items-center shadow-md mx-1 py-2 px-2 shadow-[#00000085] hover:scale-110 duration-500 text-6xl">
+              <SiMongodb/>
+              <p className="text-2xl">MongoDB</p>
+            </div>
+            <div className="flex flex-col items-center shadow-md shadow-[#00000085] hover:scale-110 p-2 duration-500 text-6xl">
+              <SiReact/>
+              <p className="text-2xl">React</p>
+            </div>
+            <div className="flex flex-col items-center shadow-md px-2 shadow-[#00000085] hover:scale-110 p-2 duration-500 text-6xl">
+              <SiExpress/>
+              <p className="text-2xl">Express</p>
+            </div>
+            <div className="flex flex-col items-center shadow-md px-2 shadow-[#00000085] hover:scale-110 p-2 duration-500 text-6xl">
+              <FaNodeJs/>
+              <p className="text-2xl">Node</p>
+            </div>
+            <div className="flex flex-col items-center shadow-md px-2 shadow-[#00000085] hover:scale-110 p-2 duration-500 text-6xl">
+              <SiNextdotjs/>
+              <p className="text-2xl">Next</p>
+            </div>
+            <div className="flex flex-col items-center shadow-md px-2 shadow-[#00000085] hover:scale-110 p-2 duration-500 text-6xl">
+              <SiTailwindcss/>
+              <p className="text-2xl">Tailwind</p>
+            </div>
+            <div className="flex flex-col items-center shadow-md px-2 shadow-[#00000085] hover:scale-110 p-2 duration-500 text-6xl">
+              <DiGit/>
+              <p className="text-2xl">Git</p>
+            </div>
+            <div className="flex flex-col items-center shadow-md px-2 shadow-[#00000085] hover:scale-110 p-2 duration-500 text-6xl">
+              <FaGithub/>
+              <p className="text-2xl">GitHub</p>
+            </div>
           </div>
         </div>
 
