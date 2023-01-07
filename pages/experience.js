dc3ff650f73c427c6cf8920c54ca9c1d1383e9b1
Anmol <anmolp476@gmail.com>
2023-01-06 20:57:52 -0500

    Completed the experience page as well, using the experience I have so far. I hope I fill it up with Internships soon :(.

--- pages/experience.js
+++ pages/experience.js
@@ -2,12 +2,51 @@ import React from 'react'
 import Layout from '../components/Layout'
 
 
-const experience = () => {
+const Experience = () => {
   return (
-    <Layout>experience</Layout>
+    <Layout>
+      <div name="The main div" >
+
+        <div name="The header container" className="flex flex-col mt-24 text-5xl w-full mb-2">
+          <p>
+            Relevant <span className="text-blue-700">Work Experience</span>,
+          </p>
+        </div>
+
+        <div name="The content div" className="w-3/5">
+
+          <div name="The python tutor div" className="w-full">
+
+            <div name="The python tutor header div" className="flex flex-row justify-between w-full text-2xl">
+              <p className="text-blue-700">Python Tutor for Varsity Tutors</p>
+              <p>Jun 2022 - Jan 2023</p>
+            </div>
+          
+            <div name="The description of python tutor div overflow-hidden">
+              <ul className="list-disc ml-8 mt-2">
+                <li className="mb-2">
+                  Experience teaching Python to students of all levels, 
+                  including beginners and those with some programming experience
+                </li>
+                <li className="mb-2">
+                  Able to adapt teaching style and 
+                  approach to the individual needs and learning styles of each student
+                </li>
+                <li className="mb-2">
+                  Able to adapt teaching style and 
+                  approach to the individual needs and learning styles of each student
+                </li>
+              </ul>
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+    </Layout>
   )
 }
 
-export default experience
+export default Experience
 
 
