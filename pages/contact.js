a538dc2323869c76d6049281e0704b0cf77c385e
Anmol <anmolp476@gmail.com>
2023-01-07 00:09:51 -0500

    Sort of made the contact me a bit more responsive too! I made everything a bit more responsive on mobile devices, and I was going to procrastinate on this lol. I will keep working on it though.

--- pages/contact.js
+++ pages/contact.js
@@ -5,15 +5,15 @@ import Layout from '../components/Layout'
 const Contact = () => {
   return (
     <Layout>
-      <div name="This is the main div" className="flex flex-row w-3/5 h-full">
+      <div name="This is the main div" className="flex flex-row w-3/5 h-full ml-24 sm:ml-0">
 
         <form method="POST" action="https://getform.io/f/870afaff-27e3-41f6-86fe-e980cd28f7ed" className="mt-4 flex flex-col items-center gap-8 justify-center w-full h-full">
 
           <div name="This will contain the header text" className="flex flex-col w-full">
-            <p  className="text-5xl">
+            <p  className="text-4xl sm:text-5xl">
               <span className="text-blue-700 ">Contact</span> Me!
             </p>
-            <div name="This contains the part of the header that is not capitalized" className="flex flex-row">
+            <div name="This contains the part of the header that is not capitalized" className="flex flex-col sm:flex-row">
               <p className="italic opacity-50 mr-1">
                 Complete the form below or shoot me an email at
               </p>
@@ -28,8 +28,8 @@ const Contact = () => {
           </div>
 
           <div name="This div will contain only the button. I covered it with a div so I can style the button easier" 
-               className="h-full w-1/5 flex flex-row justify-center mb-4 -mt-8">
-            <button className="text-white border-2 hover:bg-blue-700 hover:border-blue-700 px-4 py-3 my-8 mx-auto flex items-center">Let's Talk!</button>
+               className="h-full w-1/5 flex flex-row justify-center mb-40 sm:mb-4 -mt-8">
+            <button className="text-white border-2 hover:bg-blue-700 hover:border-blue-700 px-10 sm:px-4 py-0 sm:py-3 my-14 sm:my-8 mx-auto flex items-center">Let's Talk!</button>
           </div>
         </form>
 
