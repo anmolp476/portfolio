89ebc5e560f4c7ca54513a7ee3584b9383c29ecc
Anmol <anmolp476@gmail.com>
2023-01-10 14:10:02 -0500

    Thanks to some friends pointing it out, I had duplicate bullet points.

--- pages/experience.js
+++ pages/experience.js
@@ -14,25 +14,25 @@ const Experience = () => {
             </p>
           </div>
           <div name="The content div" className="w-4/5 sm:w-3/5">
-            <div name="The python tutor div" className="w-full"> 
+            <div name="The python tutor div" className="w-full">
               <div name="The python tutor header div" className="flex flex-row justify-between w-full text-md sm:text-2xl">
                 <p className="text-blue-700">Python Tutor for Varsity Tutors</p>
                 <p>Jun 2022 - Jan 2023</p>
               </div>
-          
+
               <div name="The description of python tutor div overflow-hidden">
                 <ul className="list-disc ml-8 mt-2">
                   <li className="mb-2">
-                    Experience teaching Python to students of all levels,
-                    including beginners and those with some programming experience
+                    Experience creating and implementing curriculum for Python courses,
+                    lesson plans and exercises
                   </li>
                   <li className="mb-2">
                     Able to adapt teaching style and
                     approach to the individual needs and learning styles of each student
                   </li>
                   <li className="mb-2">
-                    Able to adapt teaching style and
-                    approach to the individual needs and learning styles of each student
+                    Strong verbal and written communication skills,
+                    including the ability to explain technical concepts to non-technical audiences
                   </li>
                 </ul>
               </div>
